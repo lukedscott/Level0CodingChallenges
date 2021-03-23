c87fc07a4124878b1e3fc3d3f01b2abed7648a58
lukedscott <77001716+lukedscott@users.noreply.github.com>
2021-03-23 19:03:39 +0200

    Delete Task_0.5.js

--- Task_0.5.js
+++
@@ -1,8 +0,0 @@
-function getArea(side1, side2, side3) {
-var area;
-var semiperimeter;     
-    semiperimeter = 1/2 * (side1 + side2 + side3);                                
-    area = Math.sqrt(semiperimeter * (semiperimeter - side1) * (semiperimeter - side2) * (semiperimeter - side3));     
-    return area;
-}
-console.log(getArea(14, 10, 13));
