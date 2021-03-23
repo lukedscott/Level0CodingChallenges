08995fc703a7c471c0e8288a41b6e885925acbee
lukedscott <lukescott551@gmail.com>
2021-03-23 21:56:41 +0200

    more modifications

--- Task_0.9.js
+++ Task_0.9.js
@@ -1,11 +1,11 @@
-function vowels(word) {
+function vowels(str) {
 var number;
 var i;
 var outputstring = "";
 var inputstring = '';
-        inputstring = word;
+        inputstring = str;
         inputstring = inputstring.toLowerCase();
-        number = (word.length);
+        number = (str.length);
         for (i = 0; i < number; i++) {
         if ((inputstring.charAt(i) == "a" || inputstring.charAt(i) == "e" || inputstring.charAt(i) == "i" || inputstring.charAt(i) == "o" ||inputstring.charAt(i) == "u") && (outputstring.indexOf(inputstring.charAt(i)) == -1)) {
         outputstring = outputstring + inputstring.charAt(i) + ',';
