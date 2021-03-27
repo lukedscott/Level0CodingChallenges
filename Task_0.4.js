b7d5c90509e9a13fbef7e916a9db3dacd2a341d7
lukedscott <lukescott551@gmail.com>
2021-03-27 03:11:45 +0200

    modified code

--- Task_0.4.js
+++ Task_0.4.js
@@ -2,15 +2,7 @@ function evenOrOdd(number) {
     if(number %2 === 0) {             
         console.log("even");
     } else {
-<<<<<<< HEAD
         console.log("odd");
     }
 }
 evenOrOdd(5);
-=======
-        return "Odd";
-    }
-}
-
-console.log(evenOrOdd(13));
->>>>>>> 08995fc703a7c471c0e8288a41b6e885925acbee
